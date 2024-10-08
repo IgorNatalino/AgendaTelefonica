4c1fb2b544a779e31a00f5a9dab92fbcb3f0a4c7
Igor Natalino <igornatalino@gmail.com>
2024-10-08 18:45:26 -0300

    Adicionado componente de contatos e importação do JSON :zap:

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -4,6 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+import agenda from './agenda.json';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -14,10 +23,12 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
+    ContatoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
 }
